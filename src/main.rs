fa9e35781cd74d8ccdebdf0693af98bb9d96c840
Earlopain <earlopain@c5h8no4na.net>
2020-07-29 18:33:01 +0200

    [Misc] Serve files via rocket and tera_template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+use rocket_contrib::serve::StaticFiles;
+use rocket_contrib::templates::Template;
+use std::collections::HashMap;
+
+#[get("/")]
+fn compare() -> Template {
+    let mut context = HashMap::<String, String>::new();
+    context.insert(String::from("title"), String::from("TEMPLATE TITLE"));
+    Template::render("compare", context)
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .attach(Template::fairing())
+        .mount("/", routes![compare])
+        .mount("/static", StaticFiles::from("static"))
+        .launch();
 }

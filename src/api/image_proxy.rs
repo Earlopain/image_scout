86dc4eb97e12de919ff509aab24ff546c8d3f482
Earlopain <earlopain@c5h8no4na.net>
2020-08-03 21:07:25 +0200

    [Api] Return image with correct filename

--- src/api/image_proxy.rs
+++ src/api/image_proxy.rs
@@ -1,12 +1,20 @@
 use crate::db;
 use rocket::http::ContentType;
-use rocket::response::content::Content;
+use rocket::response::{Body, Response};
+use std::convert::TryInto;
+use std::io::Cursor;
 
 #[get("/image/<id>")]
-pub fn route(id: u32, conn: db::Connection) -> Content<Vec<u8>> {
+pub fn route(id: u32, conn: db::Connection) -> Response<'static> {
     let image = db::artist_post::ArtistPost::get_by_id(&id, &conn).unwrap();
-    Content(
-        ContentType::from_extension(&image.file_type).unwrap(),
-        image.blob,
-    )
+    let mut response = Response::new();
+
+    let size = image.blob.len().try_into().unwrap();
+
+    let body = Body::Sized(Cursor::new(image.blob), size);
+
+    response.set_raw_header("Content-Disposition", "inline; filename=".to_string() + &image.file_name);
+    response.set_header(ContentType::from_extension(&image.file_type).unwrap());
+    response.set_raw_body(body);
+    response
 }

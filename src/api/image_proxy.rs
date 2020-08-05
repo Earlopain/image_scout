34fe0e8fd4f7f5fcabda4d8e4bc6bd9f0ba76db9
Earlopain <earlopain@c5h8no4na.net>
2020-08-05 14:15:09 +0200

    [Porxy] Return 404 if image not found

--- src/api/image_proxy.rs
+++ src/api/image_proxy.rs
@@ -3,21 +3,32 @@ use rocket::http::ContentType;
 use rocket::response::{Body, Response};
 use std::convert::TryInto;
 use std::io::Cursor;
+use crate::db::artist_post::ArtistPost;
 
 #[get("/image/<id>")]
-pub fn route(id: i64, conn: db::Connection) -> Response<'static> {
-    let image = db::artist_post::ArtistPost::get_by_id(&id, &conn).unwrap();
+pub fn route(id: i64, conn: db::Connection) -> Option<Response<'static>> {
+    let image = get_image_from_db(id, &conn);
+    match image {
+        Ok(post) => Some(craft_response_success(post)),
+        Err(_e) => None,
+    }
+}
+
+fn craft_response_success(post: ArtistPost) -> Response<'static> {
     let mut response = Response::new();
+    let size = post.blob.len().try_into().unwrap();
 
-    let size = image.blob.len().try_into().unwrap();
-
-    let body = Body::Sized(Cursor::new(image.blob), size);
+    let body = Body::Sized(Cursor::new(post.blob), size);
 
     response.set_raw_header(
         "Content-Disposition",
-        "inline; filename=".to_string() + &image.file_name,
+        "inline; filename=".to_string() + &post.file_name,
     );
-    response.set_header(ContentType::from_extension(&image.file_type).unwrap());
+    response.set_header(ContentType::from_extension(&post.file_type).unwrap());
     response.set_raw_body(body);
     response
+}
+
+fn get_image_from_db(id: i64, conn: &db::Connection) -> Result<ArtistPost, diesel::result::Error> {
+    db::artist_post::ArtistPost::get_by_id(&id, &conn)
 }

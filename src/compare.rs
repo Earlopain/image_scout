9aeaff0ba6e5759b6534ccc14442c9592953932e
Earlopain <earlopain@c5h8no4na.net>
2020-08-06 23:39:44 +0200

    [UI] Display similar images for the upladed one

--- src/compare.rs
+++ src/compare.rs
@@ -1,27 +1,31 @@
 use db::upload_cache::UploadCache;
 use rocket::http::ContentType;
 use rocket::Data;
+use rocket_contrib::templates::tera::Context;
 use rocket_contrib::templates::Template;
 use rocket_multipart_form_data::mime;
 use rocket_multipart_form_data::{
     MultipartFormData, MultipartFormDataError, MultipartFormDataField, MultipartFormDataOptions,
 };
-use std::collections::HashMap;
 
 #[post("/compare", data = "<data>")]
 pub fn route(content_type: &ContentType, data: Data, conn: crate::Connection) -> Template {
     //TODO error template
-    let upload = insert_image_into_db(content_type, data, conn).unwrap();
-    let mut context = HashMap::<String, String>::new();
-    context.insert(String::from("title"), String::from("TEMPLATE TITLE"));
-    context.insert(String::from("uploaded_image_id"), upload.id.to_string());
+    let upload = insert_image_into_db(content_type, data, &conn).unwrap();
+
+    let similar = upload.get_similar_artist_posts(&conn).unwrap();
+
+    let mut context = Context::new();
+    context.insert("title", "TEMPLATE TITLE");
+    context.insert("uploaded_image_id", &upload.id.to_string());
+    context.insert("similar_images", &similar);
     Template::render("compare", context)
 }
 
 fn insert_image_into_db(
     content_type: &ContentType,
     data: Data,
-    conn: crate::Connection,
+    conn: &crate::Connection,
 ) -> Result<UploadCache, String> {
     let options = MultipartFormDataOptions::with_multipart_form_data_fields(vec![
         MultipartFormDataField::raw("image")
